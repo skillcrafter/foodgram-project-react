7c374c68daa433fbb8c98b087f7e1f1b340110e1
SkillCrafter2 <145963446+SkillCrafter2@users.noreply.github.com>
2024-04-25 09:09:01 +0300

    initial5

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -15,6 +15,6 @@ api_v1.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(api_v1.urls)),
-    path('auth/', include('djoser.urls')),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
