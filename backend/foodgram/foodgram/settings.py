05f3c867d5b903f50cf191e3a8bea1e29001bc9b
SkillCrafter2 <145963446+SkillCrafter2@users.noreply.github.com>
2024-04-26 16:09:28 +0300

    alpha

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = APP_DIR / 'collected_static'
+STATIC_ROOT = APP_DIR / 'backend_static'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
