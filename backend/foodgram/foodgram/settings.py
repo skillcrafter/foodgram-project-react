ab06feb19d0d43402d41a2d270eda5e8b5a5176b
skillcrafter <132369544+skillcrafter@users.noreply.github.com>
2024-04-26 23:28:34 +0300

    alpha

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -100,8 +100,8 @@ DJOSER = {
         'current_user': 'users.serializers.UserSerializer',
     },
     'PERMISSIONS': {
-          'user_list': ['rest_framework.permissions.AllowAny'],
-          'user': ['rest_framework.permissions.AllowAny'],
+        'user_list': ['rest_framework.permissions.AllowAny'],
+        'user': ['rest_framework.permissions.AllowAny'],
     }
 }
 
