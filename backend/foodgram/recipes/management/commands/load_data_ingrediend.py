30c7e6aec26970beeb5b8bf965e6f5174e9b3187
SkillCrafter2 <145963446+SkillCrafter2@users.noreply.github.com>
2024-04-26 14:09:48 +0300

    alpha

--- backend/foodgram/recipes/management/commands/load_data_ingrediend.py
+++ backend/foodgram/recipes/management/commands/load_data_ingrediend.py
@@ -4,20 +4,33 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Загрузить данные о ингредиентах в базу данных'
+    help = 'Загрузить данные об ингредиентах в базу данных'
 
     def handle(self, *args, **options):
         try:
             file_path = 'data/ingredients.json'
             with open(file_path, 'r', encoding='utf-8') as file:
                 data = json.load(file)
+                successfully_loaded = set() ########## olya 2604204
                 for item in data:
-                    ingredient, created = Ingredient.objects.get_or_create(
-                        **item)
-                    if created:
-                        self.stdout.write(self.style.SUCCESS(
-                            f'Ингредиенты "{ingredient}" успешно добавлены'))
+                    try:
+                        ingredient, created = Ingredient.objects.get_or_create(
+                            **item)
+                        if created:
+                            successfully_loaded.add((item['name'],
+                                                     item['measurement_unit']))
+                            self.stdout.write(self.style.SUCCESS(
+                                f'Ингредиенты "{ingredient}" '
+                                f'успешно добавлены'))
+                    except Exception as e:
+                        self.stderr.write(self.style.ERROR(
+                            f'Ошибка при импорте ингредиента: {e}"{ingredient}"'))
+            duplicates = [item for item in data if (item['name'],
+                                                    item['measurement_unit'])
+                          not in successfully_loaded]
+            if duplicates:
+                self.stdout.write(self.style.WARNING(
+                    f'Следующие ингредиенты не были '
+                    f'загружены из-за дубликатов: {duplicates}'))
         except FileNotFoundError:
             raise CommandError('Файл с данными не найден')
-        except Exception as e:
-            raise CommandError(f'Ошибка при импорте данных: {e}')
